c8b7402fe2a72f9f7ec239cd03bca4705368a72a
Josh Owen <erial909@hotmail.com>
2022-05-01 20:55:01 +0100

    Implemented some widget attributes and read them into our custom view.

---
+++ pixelart_imageview/src/main/java/com/joshowen/pixelart_imageview/Configuration.kt
@@ -0,0 +1,4 @@
+package com.joshowen.pixelart_imageview
+
+internal const val DEFAULT_CANVAS_WIDTH = 50
+internal const val DEFAULT_CANVAS_HEIGHT = 50
--- pixelart_imageview/src/main/java/com/joshowen/pixelart_imageview/PixelArtImageView.kt
+++ pixelart_imageview/src/main/java/com/joshowen/pixelart_imageview/PixelArtImageView.kt
@@ -4,8 +4,27 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 
-class PixelArtImageView(context: Context, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
+class PixelArtImageView(context: Context, attrs: AttributeSet?) :
+    AppCompatImageView(context, attrs) {
 
+    private var canvasWidth = 0
+    private var canvasHeight = 0
 
+    init {
+        context.theme.obtainStyledAttributes(attrs, R.styleable.PixelArtImageView, 0, 0).apply {
 
+            canvasWidth =
+                getInteger(R.styleable.PixelArtImageView_canvasWidth, DEFAULT_CANVAS_WIDTH)
+
+            canvasHeight =
+                getInteger(R.styleable.PixelArtImageView_canvasHeight, DEFAULT_CANVAS_HEIGHT)
+
+            val imageResourceId = getResourceId(R.styleable.PixelArtImageView_originalImage, -1)
+
+            if(imageResourceId == -1) {
+                throw IllegalStateException("An image needs to be provided to the PixelArtImageView.")
+            }
+            
+        }
+    }
 }
