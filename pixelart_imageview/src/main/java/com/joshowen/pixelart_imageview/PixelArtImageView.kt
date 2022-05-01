08f0e7f7bbc6778a5e83f89c49bbd48fe7a974f4
Josh Owen <erial909@hotmail.com>
2022-05-01 20:44:54 +0100

    Implemented an empty custom image view which extends AppCompatImageView

---
+++ pixelart_imageview/src/main/java/com/joshowen/pixelart_imageview/PixelArtImageView.kt
@@ -0,0 +1,11 @@
+package com.joshowen.pixelart_imageview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class PixelArtImageView(context: Context, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
+
+
+
+}
